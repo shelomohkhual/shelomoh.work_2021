17c7ed4b6188ec6c6fbe1280c3943ce0879c4718
shelomoh <shelomoh.design@gmail.com>
2020-03-27 00:12:02 +0800

    implement active Class to work partial pathName

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,23 +4,37 @@ import TopBar from "./TopBar";
 
 // const scrollDown = <p className="scroll"> SCROLL - >> </p>;
 
+const workPathsLocation = props => {
+  const currentLocation = props.pathname;
+  if (currentLocation.includes("work")) {
+    return { className: "navbar-item-link-container active" };
+  }
+};
+
 const Layout = ({ children }) => (
   <React.Fragment>
     <div className="layout-container">
       <TopBar />
-
-      {/* NAVBAR */}
       <div className="navbar">
-        <Link to="/work">
-          <p className="navbar-item-link-container">
-            <span className="navbar-item-link ">Work</span>
-          </p>
+        <Link
+          to="/work"
+          className="navbar-item-link-container"
+          activeClassName="active"
+          getProps={({ location }) => workPathsLocation(location)}
+        >
+          {/* <p className="navbar-item-link-container"> */}
+          <span className="navbar-item-link ">Work</span>
+          {/* </p> */}
         </Link>
 
-        <Link to="/contact">
-          <p className="navbar-item-link-container">
-            <span className="navbar-item-link ">Contact</span>
-          </p>
+        <Link
+          to="/contact"
+          className="navbar-item-link-container"
+          activeClassName="active"
+        >
+          {/* <p className="navbar-item-link-container"> */}
+          <span className="navbar-item-link ">Contact</span>
+          {/* </p> */}
         </Link>
       </div>
       <main className="main-container">{children}</main>
